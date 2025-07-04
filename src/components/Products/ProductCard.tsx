b09b5bc6a80b588c646d7547703fec6824f8432a
Yamir <yamcast12@gmail.com>
2025-07-04 17:15:17 -0400

    feat: add addToCart logic and user checkt to ProductCard

--- src/components/Products/ProductCard.tsx
+++ src/components/Products/ProductCard.tsx
@@ -9,30 +9,47 @@ interface ProductCardProps {
 }
 
 export const ProductCard = ({ product, onClick }: ProductCardProps) => {
+
+    // const { addToCart } = useCartStore();
+
+    // const { user } = useAuth();
+
+    // const handleAddToCart = (e: React.MouseEvent) => {
+    //     e.stopPropagation();
+    //     if (!user) {
+    //         alert("Debes iniciar sesión para agregar productos al carrito");
+    //         return;       
+    //     }
+    //     // addToCart(product.title, 1);
+    //     alert(`Producto ${product.title} agregado al carrito`);
+    // }
+
     const router = useRouter();
     return (
-        <div className="bg-white rounded-lg  cursor-pointer transition hover:scale-105">
-            <div onClick={onClick}>
-                <div className="relative w-full h-32 flex items-center justify-center"
-                >
-                    <Image src={product.imageUrl[0]} alt={product.title} width={200} height={200} className="object-contain" />
+        <div>
+            <div className="bg-white rounded-lg  cursor-pointer transition hover:scale-105">
+                <div onClick={onClick}>
+                    <div className="relative w-full h-32 flex items-center justify-center"
+                    >
+                        <Image src={product.imageUrl[0]} alt={product.title} width={200} height={200} className="object-contain" />
+                    </div>
+                    <div className="p-4">
+                        <h3 className="text-lg font-semibold text-center">
+                            {product.title}
+                        </h3>
+                        <p className="mt-2 font-bold text-xl text-center">${product.price}</p>
+                    </div>
                 </div>
-                <div className="p-4">
-                    <h3 className="text-lg font-semibold text-center">
-                        {product.title}
-                    </h3>
-                    <p className="mt-2 font-bold text-xl text-center">${product.price}</p>
-                </div>
+                <Button className="mt-1 w-full text-[14px]"
+                    style={{
+                        backgroundColor: "#D0EBFF",
+                        color: "#003D69",
+                        boxShadow: "2px 3px 2px rgba(0,0,0,0.2)"
+                    }}
+                    onClick={() => router.push('login')}>
+                    Agregar al carrito
+                </Button>
             </div>
-            <Button className="mt-1 w-full text-[14px]"
-                style={{
-                    backgroundColor: "#D0EBFF",
-                    color: "#003D69",
-                    boxShadow: "2px 3px 2px rgba(0,0,0,0.2)"
-                }}
-                onClick={() => router.push('login')}>
-                Agregar al carrito
-            </Button>
         </div>
     )
 }
