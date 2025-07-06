9ce4c8906303b8cdbc8e578732242104b755b9c9
yamirCastilloV <166874609+yamirCastilloV@users.noreply.github.com>
2025-07-06 14:16:44 -0400

    fix: solved merge conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-
 import ViewProductPage from "@/views/products/ViewProductPage";
 
 
 export default function HomePage() {
-  return <ViewProductPage/>
-
+  return (
+    <ViewProductPage />
+  )
 }
