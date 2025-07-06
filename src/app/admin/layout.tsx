abfaf08e9d0b5a50eb4fa50fd8450637786aed28
yamirCastilloV <166874609+yamirCastilloV@users.noreply.github.com>
2025-07-06 16:17:12 -0400

    fix: prevent premature redirect in protected routes by waiting for user hydration

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -10,15 +10,20 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
     const router = useRouter();
 
     useEffect(() => {
+        // Espera a que termine la hidratactión
+        if (status === 'checking') return;
+        console.log("Datos del user:", user);
         if (!user?.token) {
             router.replace('/login');
             return;
         }
         const payload = decodeJWT(user.token);
+        console.log("payload: ", payload);
         if (!payload || payload.role !== 'Admin') {
             router.replace('/');
             return;
         }
+
 
     }, [user, status, router]);
 
